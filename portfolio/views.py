fc4758ca1fc4494af282b5d5bb7983431f5fc3d2
riyadhswe <riyadhswe@gmail.com>
2021-04-25 20:02:45 +0600

    navebar and url update

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'index.html')
+    text = {
+        "name"  : "Riyadh",
+        "age"   : 24,
+        "phone" :'8801740280586',
+    }
+    return render(request,'index.html',text)
 
 def about(request):
     return render(request,'about.html')
