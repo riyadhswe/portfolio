1a60c796894d156f6b62cfcf23d67a8f219c9be6
riyadhswe <riyadhswe@gmail.com>
2021-04-29 01:20:01 +0600

    update todays task

--- index/urls.py
+++ index/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name = 'home'),
-    path('aboutus', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('aboutus', views.aboutus, name='about'),
+    path('contact', views.contactus, name='contact'),
 ]
