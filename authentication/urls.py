05b5939d6b7ba6e4119c66289c26327a8e9c704e
riyadhswe <riyadhswe@gmail.com>
2021-05-02 22:49:01 +0600

    add forgot password

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login,name = 'login'),
+    path('registration', views.registration,name = 'registration'),
+    path('forgotpassword', views.forgotpassword,name = 'forgotpassword'),
 ]
