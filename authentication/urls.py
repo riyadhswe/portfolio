8f295250b2d26fbedc5c4293152c8a2fd74841d1
riyadhswe <riyadhswe@gmail.com>
2021-05-07 21:22:37 +0600

    forgot passwordd

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login,name = 'login'),
-    path('registration', views.registration,name = 'registration'),
+    path('', views.authlogin,name = 'login'),
+    path('registration', views.authregistration,name = 'registration'),
     path('forgotpassword', views.forgotpassword,name = 'forgotpassword'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 # Create your views here.
-def login(request):
+def authlogin(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        password=request.POST['password']
+        user = authenticate(request, username = name,password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('employee.profile')
+        else:
+            print('invalid username or password')
     return render(request, 'authentication/login.html')
 
-def registration(request):
+def authregistration(request):
     return render(request, 'authentication/registration.html')
 
 def forgotpassword(request):
